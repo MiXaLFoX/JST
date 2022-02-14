d76475727748b6343ebafc4acd7663cb4b1797aa
Mikhail Lis <mixal8411@gmail.com>
2022-02-14 11:17:47 +0300

    Add lesson 4 home task

---
+++ lesson4.js
@@ -0,0 +1,25 @@
+
+// 1) Выполнить сложение различных типов(string+boolean, string+number, number+boolean)
+
+let add = 'lesson-4' + true;
+let add1 = 'lesson-4' + 1;
+let add2 = 1 + false;
+
+//2) Выполнить умножение различных типов(string * boolean, string * number, number * boolean)
+
+let mult = 'lesson-4' * true;
+let mult1 = 'lesson-4' * 2;
+let mult2 = 2 * false;
+
+//3) Выполнить деление различных типов(string/boolean, string/number, number/Boolean)
+
+let divide = 'lesson-4' / true;
+let divide1 = 'lesson-4' / 1;
+let divide2 = 1 / true;
+
+// 4) Выполнить явное преобразование(number, string, boolean)
+
+let strNum = '1';
+let num = Number(strNum);
+let str = String(num);
+let bool = Boolean(num);
--- sum.js
+++ sum.js
@@ -1,3 +1,3 @@
 let sum = (a, b) => a + b;
 
-sum(1, 4);
+console.log(sum(1, 4));
