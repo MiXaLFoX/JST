bc9db46c9c35135d5848bf8182b1fb5de7920b09
Mikhail Lis <mixal8411@gmail.com>
2022-02-16 21:18:21 +0300

    add task 1

--- lesson-5.js
+++ lesson-5.js
@@ -1,13 +1,28 @@
+// task 1
+
+function getRandomNums () {
+  let randomNums = [];
+
+  for (let i = 0; i < 3; i++) {
+    randomNums.push(Math.floor(Math.random() * (6 - 1 + 1)) + 1);
+  }
+
+  return randomNums.reduce((total, cur) => total + cur);
+}
+
+let firstPlayerThrow = getRandomNums ();
+let secondPlayerThrow = getRandomNums ();
+
+console.log(firstPlayerThrow > secondPlayerThrow ? "First player wins" : "Second player wins");
+
 // task 2
 
-const now = new Date();
 let counter = null;
 
-for (let startDate = new Date("01/01/2000"); startDate <= now; startDate.setDate(startDate.getDate() + 1)) {
+for (let startDate = new Date("2000-01-01"), now = new Date(); startDate <= now; startDate.setDate(startDate.getDate() + 1)) {
   if (startDate.getDate() === 13 && startDate.getDay() === 5) {
     counter++;
   }
 }
 
 console.log(counter);
-//console.log(fridaysThirteens.length);
